ccec86dafc7244432e86d23a54950ce86909ad75
ALIII <alimoradi7am@gmail.com>
2025-08-02 14:45:16 +0330

    Review PostConstruct and PreDestroy

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/common/CricketCoach.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/common/CricketCoach.java
@@ -1,11 +1,14 @@
 package com.luv2code.springboot.demo.mycoolapp.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
 //TIP $END$
 @Component
 @Primary
@@ -15,14 +18,23 @@ import org.springframework.stereotype.Component;
 public class CricketCoach implements Coach {
 
 
-    public CricketCoach(){
+    public CricketCoach() {
         System.out.println("In Constructor " + getClass().getSimpleName());
     }
 
     @Override
     public String getDailyWorkout() {
         return "implement dailyWorkout in CricketCoach class:-)";
-     }
+    }
 
+    @PostConstruct
+    public void doMyStartUpStuff() {
+        System.out.println("doMyStartUpStuff " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanUpStuff(){
+        System.out.println("doMyCleanUpStuff " + getClass().getSimpleName());
+    }
 
 }
